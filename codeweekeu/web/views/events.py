837e4881876c32c1006e28accbaa2f8a40f8c9ca
Alja Isakovic <ialja@me.com>
2014-02-12 21:22:06 +0100

    added basic template pages and removed db

--- codeweekeu/web/views/events.py
+++ codeweekeu/web/views/events.py
@@ -5,6 +5,15 @@ from django.template import RequestContext
 def index(request):
 
 	return render_to_response(
-		'pages/test.html',
+		'pages/index.html',
 		{'test': 'test'},
 		context_instance=RequestContext(request))
+
+def add_event(request):
+	pass
+
+def view_event(request):
+	pass
+
+def search_event(request):
+	pass
