31bd7bfc620cdbedb336d571ecfd73042154fdc0
Alja Isakovic <ialja@me.com>
2014-02-15 16:18:10 +0100

    basic add event form added
    created a django form based on proposed models; currently it doesn't post any data, just redirects the user to thankyou page after submitting

--- codeweekeu/web/views/events.py
+++ codeweekeu/web/views/events.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.http import HttpResponseRedirect
 
+from web.forms.event_form import AddEvent
 
 def index(request):
 	return render_to_response(
@@ -9,13 +11,19 @@ def index(request):
 		context_instance=RequestContext(request))
 
 def add_event(request):
-	return render_to_response(
-		'pages/add_event.html',
-		{'test': 'test'},
-		context_instance=RequestContext(request))
+	event_form = AddEvent()
+	if request.method =="POST":
+		event_form = AddEvent(data=request.POST)
+		if event_form.is_valid():
+			return HttpResponseRedirect('/thankyou/')
+	context = {"form": event_form}
+	return render_to_response("pages/add_event.html", context, context_instance=RequestContext(request))
 
 def view_event(request):
 	pass
 
 def search_event(request):
 	pass
+
+def thankyou(request):
+	return render_to_response('pages/thankyou.html')
