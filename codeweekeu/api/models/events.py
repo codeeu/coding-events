336040b279df1a2358a9f1b80cad0fa76bd55d0c
Alja Isakovic <ialja@me.com>
2014-02-18 20:18:00 +0100

    made tags mandatory and expanded the label

--- codeweekeu/api/models/events.py
+++ codeweekeu/api/models/events.py
@@ -34,7 +34,7 @@ class Event(models.Model):
 	contact_person = models.EmailField(blank=True)
 	picture = models.ImageField(upload_to='event_avatars', default='http://placehold.it/400x400', blank=True)
 	pub_date = models.DateTimeField(default=datetime.datetime.now())
-	tags=TaggableManager(blank=True)
+	tags=TaggableManager()
 	created = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now_add=True)
 
