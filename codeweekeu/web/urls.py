a9e8667232cc96016b93ed7d962f2b74420285ae
Alja Isakovic <ialja@me.com>
2014-02-26 23:51:01 +0100

    started working on list of ambassadors

--- codeweekeu/web/urls.py
+++ codeweekeu/web/urls.py
@@ -10,8 +10,9 @@ urlpatterns = patterns(
 	url(r'^add/$', 'events.add_event', name='web.add_event'),
 	url(r'^view/(?P<event_id>\d+)/(?P<slug>[-\w]+)/$', 'events.view_event', name='web.view_event'),
 	url(r'^thankyou/$', 'events.thankyou', name='web.thankyou'),
-	url(r'^guide/$', 'events.guide', name='web.guide'),
-	url(r'^login/$', 'users.login', name='web.login'),
 	url(r'^approved/(?P<country_code>\w{2,3})/$',"events.list_approved_events",name="web.list_events"),
 	url(r'^pending/(?P<country_code>\w{2,3})/$', "events.list_pending_events",name="web.pending_events"),
+	url(r'^guide/$', 'events.guide', name='web.guide'),	
+	url(r'^login/$', 'users.login', name='web.login'),
+	url(r'^ambassadors/$', 'users.ambassadors', name='web.ambassadors'),
 	)
