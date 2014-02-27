286c7c6bf5d8d77e2f1b8cad2119070bbf6b939c
Alja Isakovic <ialja@me.com>
2014-02-27 23:33:21 +0100

    getting and displaying list of ambassadors for each country
    still need to display proper profile for each ambassador

--- codeweekeu/web/processors/user.py
+++ codeweekeu/web/processors/user.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django_countries import countries
 
 def get_ambassadors():
 	ambassadors = []
@@ -6,3 +7,16 @@ def get_ambassadors():
 	for ambassador in aambassadors:
 		ambassadors.append(ambassador.profile)
 	return ambassadors
+
+def get_ambassadors_for_countries():
+	ambassadors = get_ambassadors()
+	countries_ambassadors = []
+	for code, name in list(countries):
+		readable_name = unicode(name)
+		found_ambassadors = []
+		for ambassador in ambassadors:
+			if ambassador.country == code:
+				found_ambassadors.append(ambassador)
+		countries_ambassadors.append((readable_name,found_ambassadors))
+	countries_ambassadors.sort()
+	return countries_ambassadors
