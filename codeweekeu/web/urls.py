3c160408f2edea9d5976cc7ba6566cb55076b178
Alja Isakovic <ialja@me.com>
2014-03-03 18:48:17 +0100

    removed preview url

--- codeweekeu/web/urls.py
+++ codeweekeu/web/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns
 from django.conf.urls import url
 from django.contrib import admin
-from preview_forms import AddEventFormPreview
 from web.forms.event_form import AddEvent
 
 admin.autodiscover()
@@ -19,9 +18,4 @@ urlpatterns = patterns(
 	url(r'^login/$', 'users.login', name='web.login'),
 	url(r'^ambassadors/$', 'users.ambassadors', name='web.ambassadors'),
 	 url(r'^approve_event/(?P<event_id>\d+)','events.approve_event',name='web.approve_event'),
-	)
-
-urlpatterns+= patterns(
-	'',
-	url(r'^add/$',AddEventFormPreview(AddEvent),name='web.preview_event'),
 	)
