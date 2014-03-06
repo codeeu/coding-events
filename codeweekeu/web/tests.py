53dfd81e9b23654dd209c7eedfc581a5d2f2de68
Mateja <mateja.verlic@zemanta.com>
2014-03-06 09:53:32 +0100

    More tests for web

--- codeweekeu/web/tests.py
+++ codeweekeu/web/tests.py
@@ -1,7 +1,10 @@
 import datetime
+from decimal import *
 from django.test import TestCase
 from django.db import IntegrityError
+from django_countries import countries
 from models.events import Event
+from geoposition import Geoposition
 from processors.event import get_event, create_or_update_event
 
 class EventTestCase(TestCase):
@@ -10,7 +13,8 @@ class EventTestCase(TestCase):
 							description="asdsad",location="asdsad",
 							start_date=datetime.datetime.now(),end_date=datetime.datetime.now(),
 							event_url="http://eee.com",contact_person="ss@ss.com",country="SI",
-							pub_date=datetime.datetime.now())
+							pub_date=datetime.datetime.now(),
+							tags=["tag1","tag2"])
 
 	def test_get_event(self):
 		test_event = Event.objects.get(title="asdasd")
@@ -47,4 +51,36 @@ class EventTestCase(TestCase):
 				"organizer": "some organizer"
 				}
 			test_event = create_or_update_event(**event_data)
+
+	def test_create_event_from_dictionary_with_all_required_fields(self):
+		event_data = {
+				"end_date":datetime.datetime.now(),
+				"start_date":datetime.datetime.now(),
+				"organizer": "some organizer",
+				"title": "event title",
+				"pub_date":datetime.datetime.now(),
+				}
+		test_event = create_or_update_event(**event_data)
+		self.assertEqual(2, test_event.pk)
+		self.assertEqual("event title", test_event.title)
+
+
+	def test_create_event_from_dict_with_all_fields(self):
+		event_data = {
+		"end_date":datetime.datetime.now(),
+		"start_date":datetime.datetime.now(),
+		"organizer": "some organizer",
+		"title": "event title",
+		"pub_date":datetime.datetime.now(),
+		"country": "SI",
+		"geoposition": Geoposition(46.05528,14.51444),
+		"location": "Ljubljana",
+		"tags": ["tag1", "tag2"]
+		}
+		test_event = create_or_update_event(**event_data)
+		self.assertEqual(2, test_event.pk)
+		self.assertEqual("Ljubljana", test_event.location)
+		self.assertEqual("46.05528", str(test_event.geoposition.latitude))
+		self.assertIn("tag1",test_event.tags.names())
+		self.assertIn("tag2",test_event.tags.names())
 
