00a85892eb666bca5e49618e3c46fcdf41815d21
ercchy <erika@serox.si>
2014-03-08 03:35:53 +0100

    PEP8 on urls in web app

--- codeweekeu/web/urls.py
+++ codeweekeu/web/urls.py
@@ -11,10 +11,10 @@ urlpatterns = patterns(
 	url(r'^edit/(?P<event_id>\d+)/$', 'events.edit_event', name='web.edit_event'),
 	url(r'^view/(?P<event_id>\d+)/(?P<slug>[-\w]+)/$', 'events.view_event', name='web.view_event'),
 	url(r'^thankyou/$', 'events.thankyou', name='web.thankyou'),
-	url(r'^approved/(?P<country_code>\w{2,3})/$', 'events.list_approved_events',name='web.list_events'),
-	url(r'^pending/(?P<country_code>\w{2,3})/$', 'events.list_pending_events',name='web.pending_events'),
+	url(r'^approved/(?P<country_code>\w{2,3})/$', 'events.list_approved_events', name='web.list_events'),
+	url(r'^pending/(?P<country_code>\w{2,3})/$', 'events.list_pending_events', name='web.pending_events'),
 	url(r'^guide/$', 'events.guide', name='web.guide'),	
 	url(r'^login/$', 'users.login', name='web.login'),
 	url(r'^ambassadors/$', 'users.ambassadors', name='web.ambassadors'),
-	url(r'^change_status/(?P<status>\w+)/(?P<event_id>\d+)/$', 'events.change_status',name='web.change_status'),
+	url(r'^change_status/(?P<status>\w+)/(?P<event_id>\d+)/$', 'events.change_status', name='web.change_status'),
 )
