cc99edceddad79afb181da9a018d26e80ed8999b
ercchy <erika@serox.si>
2014-03-16 23:40:10 +0100

    added request to context for purpose of 'next' attribute on login

--- codeweekeu/settings.py
+++ codeweekeu/settings.py
@@ -156,6 +156,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 	'django.contrib.messages.context_processors.messages',
 	'django.core.context_processors.request',
 	'django.core.context_processors.media',
+	'django.core.context_processors.request',
 	'django.core.context_processors.static',
 	'social.apps.django_app.context_processors.backends',
 	'social.apps.django_app.context_processors.login_redirect'
