531b7856e41aa3e010fbba22f49c8c6fb4c41cad
Mateja <mateja.verlic@zemanta.com>
2014-03-23 22:45:40 +0100

    Fixed indendation

--- codeweekeu/urls.py
+++ codeweekeu/urls.py
@@ -10,20 +10,20 @@ admin.autodiscover()
 urlpatterns = patterns(
 	'',
 	url('', include('social.apps.django_app.urls', namespace='social')),
-    url(r'^avatar/', include('avatar.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-    #Note: keep this urls last
-    url(r'', include('web.urls')),
+	url(r'^avatar/', include('avatar.urls')),
+	url(r'^admin/', include(admin.site.urls)),
+	#Note: keep this urls last
+	url(r'', include('web.urls')),
 )
 
 urlpatterns += patterns(
 	'',
 	url(r'^accounts/logout/$', 'django.contrib.auth.views.logout',
-	    {'next_page': '/'}, name='logout'),
-    url(r'^accounts/profile/$', user_profile, name='profile')
+		{'next_page': '/'}, name='logout'),
+	url(r'^accounts/profile/$', user_profile, name='profile')
 )
 
 if settings.DEBUG:
-    # static files (images, css, javascript, etc.)
-    urlpatterns += patterns('', (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
-        'document_root': settings.MEDIA_ROOT}))
+	# static files (images, css, javascript, etc.)
+	urlpatterns += patterns('', (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+		'document_root': settings.MEDIA_ROOT}))
