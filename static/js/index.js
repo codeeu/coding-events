04747a81114e4a10bed353054b1632ee853070fa
Goran Blažič <goranche@me.com>
2014-04-09 23:33:07 +0200

    Index page checks for hash in URL and zooms appropriate country, still checks for server set country if hash is not set

--- static/js/index.js
+++ static/js/index.js
@@ -126,7 +126,7 @@ var Codeweek = window.Codeweek || {};
                 var sw = results[0].geometry.viewport.getSouthWest();
                 map.map.fitBounds(results[0].geometry.viewport);
                 map.map.setCenter(results[0].geometry.location);
-				$("#country").html(current_country);
+				$("#country").html("in " + current_country);
             }
         });
     }
@@ -134,12 +134,16 @@ var Codeweek = window.Codeweek || {};
     function initialize(events, lon, lan) {
         map = createMap(events, lon, lan, 4);
         setAutocomplete();
-		console.log(window.location.hash);
-        if (location.pathname !== "/") {
-        var current_country = document.getElementById('country').innerHTML;
-        zoomCountry(current_country); 
-        }
-    }
+		if (location.hash !== '') {
+			var country = $('#' + location.hash.replace('#', '').replace('!', ''));
+			if (country.length) {
+				zoomCountry(country[0].innerText)
+			}
+		} else if (location.pathname !== "/") {
+			var current_country = document.getElementById('country').innerHTML;
+			zoomCountry(current_country);
+		}
+	}
 
     var search_events_handler = function () {
         var serch_query_input = $('#search-event'),
