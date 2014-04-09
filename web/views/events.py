23b897325059aa6bf5469969ff4614abb764e103
naisnouk <naisnouk@gmail.com>
2014-04-09 20:40:35 +0200

    removing add/search/countries from index, changed about, working on all events view

--- web/views/events.py
+++ web/views/events.py
@@ -197,11 +197,7 @@ def list_pending_events(request, country_code):
 	Display a list of pending events.
 	"""
 
-	if request.user.is_staff:
-		event_list = get_pending_events()
-		event_list = sorted(event_list, key=lambda a: a.country.code)
-	else:
-		event_list = get_pending_events(country_code=country_code)
+	event_list = get_pending_events(country_code=country_code)
 
 	country_name = unicode(dict(countries)[country_code])
 
