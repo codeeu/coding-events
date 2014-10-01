c38a7b4f856cbc1e5ffd09624e6b7be104a7f8db
Goran Blažič <goranche@me.com>
2014-10-01 22:00:33 +0200

    Added tests for country redirecting, added a regex char so slashes after country code is handled

--- web/tests/test_site.py
+++ web/tests/test_site.py
@@ -6,3 +6,17 @@ def test_unknown_URL(db, client):
 	response = client.get('/bar-foo/')
 
 	assert response.status_code == 404
+
+@pytest.mark.django_db
+def test_country_redirect(db, client):
+	# Test without a slash in the end
+	response = client.get('/AB')
+
+	assert response.status_code == 301
+	assert response['Location'][-4:] == '/#AB'
+
+	# and with one
+	response = client.get('/AB/')
+
+	assert response.status_code == 301
+	assert response['Location'][-4:] == '/#AB'
