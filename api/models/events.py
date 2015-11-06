1644fda86ba17ea98e587212efee5ba5704ff3cc
Dimitar Dimitrov <me@ddimitrov.name>
2015-11-07 00:43:23 +0200

    Restrict the name for certificate field to 70 chars
    Longer names wrap on many lines and actually break the look and feel of
    the certificate.

--- api/models/events.py
+++ api/models/events.py
@@ -79,7 +79,7 @@ class Event(models.Model):
     updated = models.DateTimeField(auto_now_add=True)
     last_report_notification_sent_at = models.DateTimeField(null=True, blank=True)
     report_notifications_count = models.IntegerField(default=0, blank=True)
-    name_for_certificate = models.CharField(max_length=255, default='', blank=True, validators=[MaxLengthValidator(255)])
+    name_for_certificate = models.CharField(max_length=255, default='', blank=True, validators=[MaxLengthValidator(70)])
     participants_count = models.IntegerField(null=True, blank=True, validators=[MinValueValidator(0)])
     average_participant_age = models.FloatField(null=True, blank=True, validators=[MinValueValidator(1)])
     percentage_of_females = models.FloatField(null=True, blank=True, validators=[MinValueValidator(0), MaxValueValidator(100)])
