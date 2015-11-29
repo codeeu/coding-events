0a3cb24b3daeadc273f9bc8e6b0926fd8ef97c7a
Dimitar Dimitrov <me@ddimitrov.name>
2015-11-29 16:12:00 +0200

    Escape special LaTeX chars in the cert holder name

--- certificates/generator.py
+++ certificates/generator.py
@@ -2,6 +2,7 @@ from os import system
 from os import unlink
 from os.path import dirname
 from os.path import realpath
+import re
 
 def generate_certificate_for(event_id, certificate_name, name_of_certificate_holder):
     resources_path             = dirname(realpath(__file__)) + '/resources/'
@@ -13,7 +14,7 @@ def generate_certificate_for(event_id, certificate_name, name_of_certificate_hol
 
     with open(generic_template_path) as template:
         personalized_certificate_content = template.read().replace(
-            '<CERTIFICATE_HOLDER_NAME>', name_of_certificate_holder)
+            '<CERTIFICATE_HOLDER_NAME>', tex_escape(name_of_certificate_holder))
 
     with open(personalized_template_path, 'w') as personalized_template:
         personalized_template.write(personalized_certificate_content.encode('utf-8'))
@@ -30,3 +31,29 @@ def generate_certificate_for(event_id, certificate_name, name_of_certificate_hol
         return False
 
     return resulting_certificate_path
+
+def tex_escape(text):
+    """
+        Escapes special chars in a text to be safely included in a LaTeX document.
+        See: http://stackoverflow.com/a/25875504/75715
+        :param text: a plain text message
+        :return: the message escaped to appear correctly in LaTeX
+    """
+    conv = {
+        '&': r'\&',
+        '%': r'\%',
+        '$': r'\$',
+        '#': r'\#',
+        '_': r'\_',
+        '{': r'\{',
+        '}': r'\}',
+        '~': r'\textasciitilde{}',
+        '^': r'\^{}',
+        '\\': r'\textbackslash{}',
+        '<': r'\textless',
+        '>': r'\textgreater',
+    }
+    replacements = (re.escape(unicode(key)) for key in sorted(conv.keys(), key = lambda item: - len(item)))
+
+    regex = re.compile('|'.join(replacements))
+    return regex.sub(lambda match: conv[match.group()], text)
